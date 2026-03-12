b3a06a229975c42dab740e0e2c63b078eb512af1
xPatryk8 <patrykzuk.pz@gmail.com>
2026-03-12 22:06:31 +0100

    Add configs

---
+++ wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+config.window_close_confirmation = "NeverPrompt"
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 16
+config.color_scheme = "Tokyo Night"
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 5,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- Finally, return the configuration to wezterm:
+return config
