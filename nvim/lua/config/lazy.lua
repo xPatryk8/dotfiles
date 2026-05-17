1b61c8b2401c2c4c9ee2a5ec52273d2584100a67
xPatryk8 <patrykzuk.pz@gmail.com>
2026-05-17 18:29:11 +0200

    nvim & wezterm: Change theme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,30 +1,40 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{ "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight-night" end },
-		require 'plugins.undotree',
-		require 'plugins.telescope',
-		require 'plugins.harpoon',
-      require 'plugins.mini',
-      require 'plugins.which-key',
-      require 'plugins.gitsigns',
-      require 'plugins.lspconfig',
-      require 'plugins.blink',
-	}
+		{
+			"EdenEast/nightfox.nvim",
+			config = function()
+				require("nightfox").setup({
+					options = {
+						transparent = true,
+					},
+				})
+				vim.cmd.colorscheme("carbonfox")
+			end,
+		},
+		require("plugins.undotree"),
+		require("plugins.telescope"),
+		require("plugins.harpoon"),
+		require("plugins.mini"),
+		require("plugins.which-key"),
+		require("plugins.gitsigns"),
+		require("plugins.lspconfig"),
+		require("plugins.blink"),
+	},
 })
