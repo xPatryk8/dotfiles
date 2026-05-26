c98fbc6fc01dcc9579f704e85d4b348a2b80ad13
xPatryk8 <patrykzuk.pz@gmail.com>
2026-05-26 19:15:24 +0200

    hypr: fix issue with screen sharing

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -20,6 +20,10 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("syncthing --no-browser")
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("waybar")
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
+	hl.exec_cmd("systemctl --user start hyprland-session.target")
 end)
 
 -------------------------------
