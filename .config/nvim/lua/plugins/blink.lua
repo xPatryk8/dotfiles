f3a9854e77cdeaaf995628442cb56519419fb7bc
xPatryk8 <patrykzuk.pz@gmail.com>
2026-06-05 19:52:39 +0200

    nvim: Add keymaps to blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,15 +1,20 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = { nerd_font_variant = 'mono' },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = 'lua' },
-  },
-  opts_extend = { "sources.default" }
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		keymap = {
+			keymap = {
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+			},
+		},
+		appearance = { nerd_font_variant = "mono" },
+		completion = { documentation = { auto_show = false } },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		fuzzy = { implementation = "lua" },
+	},
+	opts_extend = { "sources.default" },
 }
