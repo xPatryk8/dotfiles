1ee1596a276da4c217253833ae5a738d64ae7701
xPatryk8 <patrykzuk.pz@gmail.com>
2026-06-24 10:04:02 +0200

    hypr: change mpvpaper command and add zen browsee to autostart

--- .config/hypr/custom/autostart.lua
+++ .config/hypr/custom/autostart.lua
@@ -3,10 +3,11 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("kdeconnect-indicator")
 	hl.exec_cmd("syncthing --no-browser")
 	hl.exec_cmd(
-		'mpvpaper -o "input-ipc-server=/tmp/mpvpaper.sock loop loop-playlist shuffle no-audio" eDP-1 ~/Pictures/wallpapers/playlist.m3u'
+		'mpvpaper -n 45 -o "input-ipc-server=/tmp/mpvpaper.sock loop-playlist shuffle no-audio"  eDP-1 ~/Pictures/wallpapers/playlist.m3u'
 	)
 	hl.exec_cmd("~/.config/hypr/script/mpvpaper-autopause.sh")
 	hl.exec_cmd("waybar")
+	hl.exec_cmd("flatpak run app.zen_browser.zen", { workspace = "1" })
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
