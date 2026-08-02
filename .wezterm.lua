83f7c3216429363c7158c1d5ddef000d7bbdafbc
xPatryk8 <patrykzuk.pz@gmail.com>
2026-08-02 22:47:48 +0200

    wezterm: change font

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,13 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
 config.window_close_confirmation = "NeverPrompt"
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
-config.font = wezterm.font("BitstromWeraNerdFontMono")
-config.font_size = 16
+config.font = wezterm.font({ family = "Iosevka" })
+config.font_size = 19
 config.color_scheme = "Carbonfox (Gogh)"
 
 config.window_background_opacity = 0.55
@@ -35,5 +29,4 @@ config.keys = {
 	},
 }
 
--- Finally, return the configuration to wezterm:
 return config
